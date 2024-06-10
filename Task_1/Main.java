4679acf93d4e8c68932bd10938acae3344faeec9
Kondaskar bhavesh ulhas <bkondaskar@gmail.com>
2024-06-10 10:02:02 +0000

    task_1

--- Task_1/Main.java
+++ Task_1/Main.java
@@ -1,60 +1,49 @@
 import java.util.Random;
 import java.util.Scanner;
-public class Main
-{
-  public static void main (String[]args) throws InterruptedException
-    {
-    	Random rd = new Random ();
-        	Scanner sc = new Scanner (System.in);
-            	int score = 0;
-                System.out.println("Number guess game !");
-                System.out.println("You have 3 Attemptto win");
-                	while (true)
-                    	  {
-                          		int comp = rd.nextInt (100);
 
-                                		int i = 3;
-                                        		while (i > 0)
-                                                		  {
-                                                          			System.out.println ("Your Attempt:" + i);
-                                                                    			System.out.print("Enter your choice:");
-                                                                                			int user = sc.nextInt ();
-                                                                                            			  i--;
-                                                                                                          			if (user > comp)
-                                                                                                                    			  {
-                                                                                                                                  				System.out.println ("your choice is high");
-                                                                                                                                                			  }
-                                                                                                                                                              			else if (user < comp)
-                                                                                                                                                                        			  {
-                                                                                                                                                                                      				System.out.println ("your choice is low");
-                                                                                                                                                                                                    			  }
-                                                                                                                                                                                                                  			else
-                                                                                                                                                                                                                            			  {
-                                                                                                                                                                                                                                          				System.out.println ("Correct answer you won!");
-                                                                                                                                                                                                                                                        				score++;
-                                                                                                                                                                                                                                                                        				break;
-                                                                                                                                                                                                                                                                                        			  }
-                                                                                                                                                                                                                                                                                                      			Thread.sleep (2000);
-                                                                                                                                                                                                                                                                                                                			System.out.print ("\033[H\033[2J");
-                                                                                                                                                                                                                                                                                                                            			System.out.flush ();
+public class Main {
+	public static void main(String[] args) throws InterruptedException {
+		Random rd = new Random();
+		Scanner sc = new Scanner(System.in);
+		int score = 0;
+		System.out.println("Number guess game !");
+		System.out.println("You have 3 Attemptto win");
+		while (true) {
+			int comp = rd.nextInt(100);
 
-                                                                                                                                                                                                                                                                                                                                        		  }
-                                                                                                                                                                                                                                                                                                                                                  		System.out.println ("Your score:" + score);
-                                                                                                                                                                                                                                                                                                                                                        		System.out.print ("Do you want to continue (press 1):");
+			int i = 3;
+			while (i > 0) {
+				System.out.println("Your Attempt:" + i);
+				System.out.print("Enter your choice:");
+				int user = sc.nextInt();
+				i--;
+				if (user > comp) {
+					System.out.println("your choice is high");
+				} else if (user < comp) {
+					System.out.println("your choice is low");
+				} else {
+					System.out.println("Correct answer you won!");
+					score++;
+					break;
+				}
+				Thread.sleep(2000);
+				System.out.print("\033[H\033[2J");
+				System.out.flush();
 
-                                                                                                                                                                                                                                                                                                                                                                		int a = sc.nextInt ();
-                                                                                                                                                                                                                                                                                                                                                                        		if (a == 1)
-                                                                                                                                                                                                                                                                                                                                                                                		  {
-                                                                                                                                                                                                                                                                                                                                                                                          			System.out.print ("\033[H\033[2J");
-                                                                                                                                                                                                                                                                                                                                                                                                    			System.out.flush ();
-                                                                                                                                                                                                                                                                                                                                                                                                                			continue;
+			}
+			System.out.println("Your score:" + score);
+			System.out.print("Do you want to continue (press 1):");
 
-                                                                                                                                                                                                                                                                                                                                                                                                                            		  }
-                                                                                                                                                                                                                                                                                                                                                                                                                                      		else
-                                                                                                                                                                                                                                                                                                                                                                                                                                            		  {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                      			break;
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                		  }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                          	  }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                              sc.close();
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
+			int a = sc.nextInt();
+			if (a == 1) {
+				System.out.print("\033[H\033[2J");
+				System.out.flush();
+				continue;
+
+			} else {
+				break;
+			}
+		}
+		sc.close();
+	}
+}
